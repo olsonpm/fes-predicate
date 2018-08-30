dd1db5768393ca0279c795370003c25c67a6758e
Philip Olson <philip.olson@pm.me>
2018-08-30 14:17:28 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//
+// don't edit this file.  It is generated via `fes write-entries`
+//
+// check whether it's in sync with your lib directory via `fes checkup`
+//
+
+import { createUtility } from 'fes'
+
+import isFalsey_definition from './lib/is-falsey'
+const isFalsey = createUtility(isFalsey_definition)
+
+export {
+  isFalsey,
+}
